9660fcb70a9d48ac72c0ad0ac522b718379478b6
André Mourão <andre.b.mourao@gmail.com>
2019-03-19 15:50:32 +0000

    Added basic admin page

---
+++ powersong/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from powersong.models import *
+
+admin.site.register(Athlete)
+admin.site.register(ListenerSpotify)
+admin.site.register(Listener)
+admin.site.register(ActivitiesToIgnore)
+admin.site.register(Activity)
+admin.site.register(PowerUser)
+admin.site.register(Tags)
+admin.site.register(Artist)
+admin.site.register(Song)
+admin.site.register(FlaggedArtist)
+admin.site.register(FlaggedSong)
+admin.site.register(Effort)
+
+
