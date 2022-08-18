64fb9de834c4fe13bf0228efbe8770a61e7953eb
André Mourão <andre.b.mourao@gmail.com>
2022-08-18 19:15:39 +0000

    Minor upgrade fixes

--- powersong/wsgi.py
+++ powersong/wsgi.py
@@ -1,10 +1,10 @@
 """
-WSGI config for powersong project.
+WSGI config for careceiver_server project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
@@ -12,14 +12,17 @@ import site
 import sys
 
 
-site.addsitedir('/home/amourao/veffortly/')
-site.addsitedir('/home/amourao/veffortly/lib/python3.8/site-packages/')
+site.addsitedir('/home/amourao/effortly/effortly/')
+site.addsitedir('/home/amourao/effortly/effortly/lib/python3.10/site-packages/')
 
-sys.path.append('/home/amourao/powersong')
+sys.path.append('/home/amourao/effortly/')
+sys.path.append('/home/amourao/effortly/effortly/bin')
 
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ["DJANGO_SETTINGS_MODULE"] = "powersong.settings"
+os.environ["DJANGO_SETTINGS_MODULE"] = 'powersong.settings'
+
+import django.core.handlers.wsgi
 
 application = get_wsgi_application()
