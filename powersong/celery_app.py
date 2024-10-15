260f122e6d386efc04b01698b219c076bc71bd06
André Mourão <andre.b.mourao@gmail.com>
2024-10-15 21:38:06 +0100

    Re-add rate limits

--- powersong/celery_app.py
+++ powersong/celery_app.py
@@ -16,9 +16,9 @@ app.add_defaults({
 
 app.autodiscover_tasks()
 
-#app.control.rate_limit('powersong.tasks.strava_task', '40/m')
-#app.control.rate_limit('powersong.tasks.lastfm_task', '250/m')
-#app.control.rate_limit('powersong.tasks.spotify_task', '250/m')
+app.control.rate_limit('powersong.tasks.strava_task', '40/m')
+app.control.rate_limit('powersong.tasks.lastfm_task', '250/m')
+app.control.rate_limit('powersong.tasks.spotify_task', '250/m')
 
 @app.task(bind=True)
 def debug_task(self):
